8ea42392849e74bbba2530f0a624dadaddf76aa0
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-10 13:28:39 +1000

    Added shootable bullets

--- main.lua
+++ main.lua
@@ -1,17 +1,48 @@
+canShoot = true
+canShootMax = 0.2
+canShootTimer = canShootMax
+bulletImage = nil
+
+bullets = {}
 player = {x = 200, y = 710, speed = 150, image = nil}
 
 function love.load(a)
+    bulletImage = love.graphics.newImage("assets/bullet_2_orange.png")
     player.image = love.graphics.newImage("assets/Aircraft_03.png")
 end
 
 function love.update(td)
     tryKill()
     tryMove(td)
+    tryShoot(td)
+    positionBullets(td)
+end
+
+function love.draw(td)
+    drawPlayer()
+    drawBullets()
 end
 
 function tryKill()
     if love.keyboard.isDown("escape") then
         love.event.push("quit")
+    end
+end
+
+function tryShoot(td)
+    if not canShoot then
+        canShootTimer = canShootTimer - (1 * td)
+
+        if canShootTimer < 0 then
+            canShoot = true
+        end
+    end
+
+    if love.keyboard.isDown(' ') and canShoot then
+        bullet = {x = player.x + (player.image:getWidth() / 2), y = player.y, image = bulletImage, speed = 140}
+        table.insert(bullets, bullet)
+        canShoot = false
+        canShootTimer = canShootMax
     end
 end
 
@@ -27,6 +58,22 @@ function tryMove(td)
     end
 end
 
-function love.draw(td)
+function positionBullets(td)
+    for i, b in ipairs(bullets) do
+        b.y = b.y - (b.speed * td)
+
+        if b.y < 0 then
+            table.remove(bullets, i)
+        end
+    end
+end
+
+function drawPlayer()
     love.graphics.draw(player.image, player.x, player.y)
+end
+
+function drawBullets()
+    for i, b in ipairs(bullets) do
+        love.graphics.draw(b.image, b.x, b.y)
+    end
 end
